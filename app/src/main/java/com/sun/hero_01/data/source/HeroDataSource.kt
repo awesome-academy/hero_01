94b32f209f7ee28b21e89d0bbe654e4fb647c96c
Tuanlvt <le.vu.tan.tuan@sun-asterisk.com>
2021-05-18 15:40:58 +0700

    Revert "Refs [TASK] [36118] Hiển thị danh sách hero yêu thích"

--- app/src/main/java/com/sun/hero_01/data/source/HeroDataSource.kt
+++ app/src/main/java/com/sun/hero_01/data/source/HeroDataSource.kt
@@ -1,15 +1,10 @@
 package com.sun.hero_01.data.source
 
-import com.sun.hero_01.data.model.Favourite
 import com.sun.hero_01.data.source.remote.OnFetchDataJsonListener
 
 interface HeroDataSource {
 
-    interface Local {
-        fun insertHero(hero: Favourite): Boolean
-        fun deleteHero(idHero: String): Boolean
-        fun getAllHero(): MutableList<Favourite>
-        fun checkFavorite(idHero: Int): Boolean
+    interface Local{
     }
 
     interface Remote {
